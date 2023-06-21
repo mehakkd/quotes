4908f0ca9cee5d4b95024874d8d85de810bdf351
Mehakkd <mehakdhaliwal5@gmail.com>
2023-06-21 12:32:41 -0400

    Modigied

--- src/App.js
+++ src/App.js
@@ -14,12 +14,6 @@ const App = () => {
   const quotes = [
     '"More tea, please!" - Uncle Iroh, Avatar the Last Airbender',
     '"Delight is like the butterflies flying around and landing on the flower that is joy." -Ross Gay, This American Life',
-    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.'
   ]
   
   const [selected, setSelected] = useState(0);
@@ -37,7 +31,7 @@ const App = () => {
   return (
     <div className= "App-header">
       
-        <h1>Quote of the Day</h1>
+        <h1>A Collection of Quotes</h1>
         {quotes[selected]}
 
         <div>
